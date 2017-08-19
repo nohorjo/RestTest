8b8dd950ed7e938f2538e909ce59656a680a4bb9
nohorjo <nohorjo@gmail.com>
2017-08-19 10:28:03 +0100

    Split project into a hierarchy

--- src/nohorjo/resttest/Main.java
+++ src/nohorjo/resttest/Main.java
@@ -14,7 +14,7 @@ import nohorjo.resttest.utils.PropertiesUtils;
 
 public class Main {
 
-	private static final Map<String, String> results = new HashMap<>();
+	private static final Map<String, Map<String, String>> results = new HashMap<>();
 
 	private static final FilenameFilter JS_ONLY = new FilenameFilter() {
 		@Override
@@ -28,18 +28,35 @@ public class Main {
 		}
 	};
 
+	private static final FilenameFilter DIRS = new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			if (new File(dir, name).isDirectory()) {
+				System.out.println("Found " + name);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	};
+
 	public static void main(String[] args) {
 		int returnCode = 0;
 		try {
 			String project = args[0];
 			PropertiesUtils.loadEnvAndProperties(project);
-			File[] scripts = new File(project).listFiles(JS_ONLY);
-			for (File script : scripts) {
-				int rc = processScript(script);
-				if (returnCode == 0) {// if any test fails return 1
-					returnCode = rc;
+
+			File[] testSuites = new File(project).listFiles(DIRS);
+			for (File ts : testSuites) {
+				File[] scripts = ts.listFiles(JS_ONLY);
+				for (File testCasescript : scripts) {
+					int rc = processScript(ts.getName(), testCasescript);
+					if (returnCode == 0) {// if any test fails return 1
+						returnCode = rc;
+					}
 				}
 			}
+
 		} catch (Throwable e) {
 			e.printStackTrace();
 			returnCode = 1;
@@ -47,13 +64,17 @@ public class Main {
 
 		System.out.println("\n\nDone!");
 		System.out.println("\n\nSummary:");
-		for (String test : results.keySet()) {
-			System.out.println(test + "\t" + results.get(test));
+		for (String testSuite : results.keySet()) {
+			System.out.println(testSuite);
+			Map<String, String> testSuiteResults = results.get(testSuite);
+			for (String result : testSuiteResults.keySet()) {
+				System.out.println("\t" + testSuiteResults.get(result));
+			}
 		}
 		System.exit(returnCode);
 	}
 
-	private static int processScript(File script) throws Throwable {
+	private static int processScript(String testSuiteName, File script) throws Throwable {
 		int returnCode = 0;
 		String name = script.getName();
 		String contents = new String(Files.readAllBytes(script.toPath()));
@@ -75,8 +96,14 @@ public class Main {
 			returnCode = 1;
 			System.err.println(e.getMessage().replace("<eval>", name));
 		}
-		results.put(name, String.format("%s\t%d milllis", returnCode == 0 ? "passed" : "failed",
+
+		Map<String, String> m = results.get(testSuiteName);
+		if (m == null) {
+			m = new HashMap<>();
+		}
+		m.put(name, String.format("%s\t%d milllis", returnCode == 0 ? "passed" : "failed",
 				System.currentTimeMillis() - start));
+		results.put(testSuiteName, m);
 		return returnCode;
 	}
 
@@ -90,7 +117,8 @@ public class Main {
 		boolean foundSecond = true;// first one is BaseScript
 		for (StackTraceElement ste : e.getStackTrace()) {
 			if (ste.getFileName().equals("<eval>") && (foundSecond = !foundSecond)) {
-				System.err.printf("Error in %s:%d\t%s\n", name, ste.getLineNumber(), e.getMessage());
+				System.err.printf("Error in %s:%s:%d\t%s\n", name, ste.getMethodName(), ste.getLineNumber(),
+						e.getMessage());
 				return;
 			}
 		}
